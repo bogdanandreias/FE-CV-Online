003139de44de19575e0fe9ea6c472649f9830c71
Bertea Fabian <fabian.bertea@yahoo.com>
2021-05-19 11:17:06 +0300

    Created initial CV component.
    Also deleted HelloWorld reference from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CurriculumVitae />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CurriculumVitae from './components/CurriculumVitae.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CurriculumVitae
   }
 }
 </script>
