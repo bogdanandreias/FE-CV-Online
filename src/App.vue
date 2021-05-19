80bc0b9a54ec1054b3cc63fe1c42e6048be0e04f
Bertea Fabian <fabian.bertea@yahoo.com>
2021-05-19 13:24:40 +0300

    Added Content and About for CV.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
-  <CurriculumVitae />
+    <CurriculumVitaeContent />
+    <CurriculumVitaeAbout />
 </template>
 
 <script>
-import CurriculumVitae from './components/CurriculumVitae.vue'
+import CurriculumVitaeContent from "./components/CurriculumVitaeContent.vue";
+import CurriculumVitaeAbout from "./components/CurriculumVitaeAbout.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    CurriculumVitae
-  }
-}
+    CurriculumVitaeContent,
+    CurriculumVitaeAbout,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  display: block;
 }
 </style>
