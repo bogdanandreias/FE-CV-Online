f687920d22d60b1ff9d13954d7f1806f37fd7075
Bertea Fabian <fabian.bertea@yahoo.com>
2021-05-20 10:31:47 +0300

    Updated App.vue with created components.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
     <CurriculumVitaeContent />
     <CurriculumVitaeAbout />
+    <CurriculumVitaeServices />
+    <CurriculumVitaeContact />
 </template>
 
 <script>
 import CurriculumVitaeContent from "./components/CurriculumVitaeContent.vue";
 import CurriculumVitaeAbout from "./components/CurriculumVitaeAbout.vue";
+import CurriculumVitaeServices from "./components/CurriculumVitaeServices.vue";
+import CurriculumVitaeContact from "./components/CurriculumVitaeContact.vue";
 
 export default {
   name: "App",
   components: {
     CurriculumVitaeContent,
     CurriculumVitaeAbout,
+    CurriculumVitaeServices,
+    CurriculumVitaeContact
   },
 };
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  min-height: 100%;
+}
+
 #app {
   min-height: 100vh;
   display: block;
